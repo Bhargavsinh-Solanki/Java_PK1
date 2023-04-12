cf2aa825cf6e8210b3a19fdee819a7ac6a2fa941
Bhargavsinh Solanki <bhargavsinh.solanki@uni-konstanz.de>
2023-04-12 19:46:10 +0200

    file updated with for loop example

--- DoWhile.java
+++ DoWhile.java
@@ -7,5 +7,22 @@ public class DoWhile {
             i++;
         }
         while (i < 5);
+
+
+        //for loop for Java
+        for (int j = 0; j < 5; j++) {
+            System.out.println(j);
+        }
+
+        //nested loop
+        for (int l = 0; l <= 2; l++){
+            System.out.println("Outer: " + l); //executes 2 times
+
+        //Inner loo
+            for (int k = 0; k <= 3; k++) {
+                System.out.println("Inner: " + k);
+            }
+        }
+
     }
 }
